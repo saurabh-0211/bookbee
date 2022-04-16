67b0af6ff818a5e1fdbae6f6540e4ac120ea9f0b
vivekvag <vivekvag281@gmail.com>
2022-04-16 12:39:12 +0530

    Changelog 2.2.7 Search Param Created for BookBeed

--- client/App.js
+++ client/App.js
@@ -1,13 +1,11 @@
 import ReactDOM from 'react-dom';
-import Book from './Book';
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me</h1>
-      <Book name="Luna" animal="Dog" breed="Havanese" />
-      <Book name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Book name="Beam" animal="Dog" breed="Wheaten Terrier" />
+      <SearchParams />
     </div>
   );
 };
