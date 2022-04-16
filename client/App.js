1f2270d1bed780ad66a6212c13f36188c5a21ea0
saurabh-0211 <saurabhdubey0211@gmail.com>
2022-04-16 18:15:43 +0530

    login form

--- client/App.js
+++ client/App.js
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom';
 import Book from './Book';
+import LoginForm from './components/auth/login';
+
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me</h1>
+      <LoginForm></LoginForm>
       <Book name="Luna" animal="Dog" breed="Havanese" />
       <Book name="Pepper" animal="Bird" breed="Cockatiel" />
       <Book name="Beam" animal="Dog" breed="Wheaten Terrier" />
