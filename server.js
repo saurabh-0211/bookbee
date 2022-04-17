c460ed2b5c261bd5a0414ed1d0eace5d14c00bf5
saurabh-0211 <saurabhdubey0211@gmail.com>
2022-04-17 14:24:26 +0530

    cors enabled

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ const booksRoute = require('./routes/books')
 // middlewares
 app.use(express.json());
 app.use(morgan('tiny'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:1234"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(`${api}/users`,usersRoute)
 app.use(`${api}/books`,booksRoute)
 app.use(`/public/uploads`,express.static(__dirname + '/public/uploads'))
