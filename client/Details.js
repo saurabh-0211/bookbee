2fe5f954c1a7e6a7c359307dd8f501840b89476c
vivekvag <vivekvag281@gmail.com>
2022-04-21 00:47:55 +0530

    Changelog 3.3.2 Books Detail page is created... Only Styling is leftd

--- client/Details.js
+++ client/Details.js
@@ -1,5 +1,39 @@
-const Details = () => {
-  return <h2>Hello Vivek</h2>;
-};
+import { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
-export default Details;
+class Details extends Component {
+  constructor() {
+    super();
+
+    this.state = { loading: true };
+  }
+  async componentDidMount() {
+    const res = await fetch(
+      `http://localhost:3000/bookbee/books?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false
+        },
+        json[0]
+      )
+    );
+  }
+  render() {
+    const { stream, subject, bookName, publisher, authors, rating } =
+      this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{`${subject} -${bookName}`}</h1>
+          <h2>{`${stream} - ${authors} - ${publisher}`}</h2>
+          <button>{rating}</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
