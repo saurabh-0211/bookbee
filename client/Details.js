349a4e48383d399e060ff818b5309dba6eee281b
vivekvag <vivekvag281@gmail.com>
2022-04-21 12:08:10 +0530

    Changelog 3.4 Books Featured image added.

--- client/Details.js
+++ client/Details.js
@@ -2,11 +2,8 @@ import { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
 class Details extends Component {
-  constructor() {
-    super();
+  state = { loading: true };
 
-    this.state = { loading: true };
-  }
   async componentDidMount() {
     const res = await fetch(
       `http://localhost:3000/bookbee/books?id=${this.props.match.params.id}`
@@ -22,10 +19,17 @@ class Details extends Component {
     );
   }
   render() {
-    const { stream, subject, bookName, publisher, authors, rating } =
+    if (this.state.loading) {
+      return <h2>loading....</h2>;
+    }
+
+    const { stream, subject, bookName, publisher, authors, image, rating } =
       this.state;
     return (
       <div className="details">
+        <div className="carousel">
+          <img src={image} alt={bookName} />
+        </div>
         <div>
           <h1>{`${subject} -${bookName}`}</h1>
           <h2>{`${stream} - ${authors} - ${publisher}`}</h2>
