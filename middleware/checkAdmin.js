e0352bf79cfc95a7d93cfe16c73a67e30bedc155
Eshaan-commits-159 <61916584+Eshaan-commits-159@users.noreply.github.com>
2022-05-01 19:32:13 +0530

    added checkadmin middleware

---
+++ middleware/checkAdmin.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+// const { User } = require('../models/users');
+const asyncHandler = require('express-async-handler');
+const checkAuth = require('checkAuth.js');
+require('dotenv/config');
+
+const checkAdmin = asyncHandler(checkAuth,async(req,res,next)=>{
+  if(req.user && req.user.isAdmin){
+    next()
+  }else{
+    res.status(401)
+    throw new Error('only admins are allowed to do this')
+  }
+})
+
+module.exports = {checkAdmin}
+
+
+
