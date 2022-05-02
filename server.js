799d4d14102bfb9589d691fce57faf76e7c45db3
vivekvag <vivekvag281@gmail.com>
2022-05-02 12:59:20 +0530

    Changelog 3.6.4 Commented Backend Code which was causing error..

--- server.js
+++ server.js
@@ -1,48 +1,46 @@
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const notFound = require('./middleware/errorHandler')
-const errorHandler = require('./middleware/errorHandler')
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+// const notFound = require('./middleware/errorHandler')
+// const errorHandler = require('./middleware/errorHandler')
 
-require('dotenv/config')
-const api = process.env.API_URL
+require('dotenv/config');
+const api = process.env.API_URL;
 
-const usersRoute = require('./routes/users')
-const booksRoute = require('./routes/books')
-
+const usersRoute = require('./routes/users');
+const booksRoute = require('./routes/books');
 
 // middlewares
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
 // app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:1234"); 
+//   res.header("Access-Control-Allow-Origin", "http://localhost:1234");
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //   next();
 // });
-app.use(`${api}/users`,usersRoute)
-app.use(`${api}/books`,booksRoute)
-app.use(notFound)
-app.use(errorHandler)
-app.use(`/public/uploads`,express.static(__dirname + '/public/uploads'))
+app.use(`${api}/users`, usersRoute);
+app.use(`${api}/books`, booksRoute);
+// app.use(notFound);
+// app.use(errorHandler);
+app.use(`/public/uploads`, express.static(__dirname + '/public/uploads'));
 
-
-
-mongoose.connect(process.env.CONNECTION_STRING, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  dbName: 'bookbee'
-})
-.then(() => {
-  console.log('database connected successfully')
-})
-.catch((err) => {
-  console.log(err);
-})
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'bookbee'
+  })
+  .then(() => {
+    console.log('database connected successfully');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log(api);
   console.log('server is running http://localhost:3000');
-})
+});
