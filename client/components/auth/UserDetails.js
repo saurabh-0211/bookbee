d5ea91827dc6fddc4660ea9efb712254750fb4d7
vivekvag <vivekvag281@gmail.com>
2022-05-02 20:33:22 +0530

    Changelog 3.6.7 Error handled in  Register Form. Styling is left

--- client/components/auth/UserDetails.js
+++ client/components/auth/UserDetails.js
@@ -3,12 +3,80 @@ import { Container, Typography, Grid, TextField, Button } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const theme = createTheme();
-
+let newUser = true;
 class UserDetails extends Component {
+  state = {
+    errors: {}
+  };
+
+  checkExisting = ({ email, password }) => {
+    const config = {
+      headers: {
+        'content-type': 'application/json'
+      }
+    };
+
+    //request body
+    const body = JSON.stringify({ email, password });
+
+    axios
+      .post('http://localhost:3000/bookbee/users/register', body, config)
+      .then((res) => {
+        console.log(res.data);
+        newUser = false;
+        this.state.errors['email'] = "Don't try to fool us.";
+      })
+      .catch((err) => {
+        this.setState({ msg: err.response.data });
+      });
+  };
+
   continue = (e) => {
     e.preventDefault();
-    this.props.nextStep();
+    if (this.validateForm()) {
+      const { email, password } = this.props.values;
+
+      const user = {
+        email: email,
+        password: password
+      };
+
+      this.props.nextStep();
+    }
   };
+
+  validateForm() {
+    let email = this.props.values.email;
+    let password = this.props.values.password;
+    let errors = {};
+    let formIsValid = true;
+
+    if (!email) {
+      formIsValid = false;
+      errors['email'] = '*Please enter your email-ID.';
+    }
+
+    if (typeof email !== 'undefined') {
+      //regular expression for email validation
+      var pattern = new RegExp(
+        /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+      );
+      if (!pattern.test(email)) {
+        formIsValid = false;
+        errors['email'] = '*Please enter valid email-ID.';
+      }
+    }
+
+    if (!password) {
+      formIsValid = false;
+      errors['password'] = '*Please enter your password.';
+    }
+
+    this.setState({
+      errors: errors
+    });
+    return formIsValid;
+  }
 
   render() {
     const { values, handleChange } = this.props;
@@ -26,6 +94,7 @@ class UserDetails extends Component {
           />
           <br />
           <br />
+          <div className="errorMsg">{this.state.errors.email}</div>
           <TextField
             placeholder="Username"
             label="Username"
@@ -47,6 +116,7 @@ class UserDetails extends Component {
           />
           <br />
           <br />
+          <div className="errorMsg">{this.state.errors.password}</div>
           <Button
             onClick={this.continue}
             type="submit"
