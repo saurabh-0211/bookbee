7f40bd0aec61fa003987e3aee4d2b96a569ca68f
vivekvag <vivekvag281@gmail.com>
2022-05-05 22:27:42 +0530

    Changelog 3.7.12 Details component half styling done.

--- client/Details.js
+++ client/Details.js
@@ -3,6 +3,7 @@ import { withRouter } from 'react-router-dom';
 import Rating from '@mui/material/Rating';
 import LinearProgress from '@mui/material/LinearProgress';
 import Box from '@mui/material/Box';
+import Review from './Review';
 class Details extends Component {
   state = { loading: true, value: 0 };
 
@@ -30,96 +31,105 @@ class Details extends Component {
       sum += numRatings[key];
     }
     return (
-      <div className="details">
-        <div className="book-img">
-          <img src={image} alt={bookName} />
-        </div>
-        <div className="book-details">
-          <div className="book-rating">
-            <Rating name="read-only" size="small" value={rating} precision={0.5} readOnly />
-            <span>
-              {rating} Ratings & {numReviews} Reviews
-            </span>
+      <div>
+        <div className="details">
+          <div className="book-img">
+            <img src={image} alt={bookName} />
           </div>
-          <h1>{bookName}</h1>
-          <div className="list">
-            <span>Details</span>
-            <div className="book-info">
-              <div className="item">
-                <span>Stream</span>
-                <h3 style={{ textTransform: 'capitalize' }}>{stream}</h3>
-              </div>
-              <div className="item">
-                <span>Subject</span>
-                <h3>{subject}</h3>
-              </div>
-              <div className="item">
-                <span>Author</span>
-                <h3>{authors}</h3>
-              </div>
-              <div className="item">
-                <span>Publisher</span>
-                <h3>{publisher}</h3>
-              </div>
+          <div className="book-details">
+            <div className="book-rating">
+              <Rating
+                name="read-only"
+                size="small"
+                value={rating.toFixed(1)}
+                precision={0.5}
+                readOnly
+              />
+              <span>
+                {rating.toFixed(1)} Ratings & {numReviews} Reviews
+              </span>
             </div>
-          </div>
-          <div className="list">
-            <span>Ratings</span>
-            <div className="book-ratings">
-              <div className="total-ratings">
-                <h1>{rating.toFixed(1)} ★</h1>
-                <h3>{sum} Ratings</h3>
-              </div>
-              <div className="star-ratings">
-                <div className="star">
-                  <span>5 ★</span>
-                  <LinearProgress
-                    className="progress"
-                    variant="determinate"
-                    value={sum ? (numRatings.five / sum) * 100 : 0}
-                  />
-                  <span>{numRatings.five}</span>
+            <h1>{bookName}</h1>
+            <div className="list">
+              <span>Details</span>
+              <div className="book-info">
+                <div className="item">
+                  <span>Stream</span>
+                  <h3 style={{ textTransform: 'capitalize' }}>{stream}</h3>
                 </div>
-                <div className="star">
-                  <span>4 ★</span>
-                  <LinearProgress
-                    className="progress"
-                    variant="determinate"
-                    value={sum ? (numRatings.four / sum) * 100 : 0}
-                  />
-                  <span>{numRatings.four}</span>
+                <div className="item">
+                  <span>Subject</span>
+                  <h3>{subject}</h3>
                 </div>
-                <div className="star">
-                  <span>3 ★</span>
-                  <LinearProgress
-                    className="progress"
-                    variant="determinate"
-                    value={sum ? (numRatings.three / sum) * 100 : 0}
-                  />
-                  <span>{numRatings.three}</span>
+                <div className="item">
+                  <span>Author</span>
+                  <h3>{authors}</h3>
                 </div>
-                <div className="star">
-                  <span>2 ★</span>
-                  <LinearProgress
-                    className="progress"
-                    variant="determinate"
-                    value={sum ? (numRatings.two / sum) * 100 : 0}
-                  />
-                  <span>{numRatings.two}</span>
-                </div>
-                <div className="star">
-                  <span>1 ★</span>
-                  <LinearProgress
-                    className="progress"
-                    variant="determinate"
-                    value={sum ? (numRatings.one / sum) * 100 : 0}
-                  />
-                  <span>{numRatings.one}</span>
+                <div className="item">
+                  <span>Publisher</span>
+                  <h3>{publisher}</h3>
                 </div>
               </div>
             </div>
+            <div className="list">
+              <span>Ratings</span>
+              <div className="book-ratings">
+                <div className="total-ratings">
+                  <h1>{rating.toFixed(1)} ★</h1>
+                  <h3>{sum} Ratings</h3>
+                </div>
+                <div className="star-ratings">
+                  <div className="star">
+                    <span>5 ★</span>
+                    <LinearProgress
+                      className="progress"
+                      variant="determinate"
+                      value={sum ? (numRatings.five / sum) * 100 : 0}
+                    />
+                    <span>{numRatings.five}</span>
+                  </div>
+                  <div className="star">
+                    <span>4 ★</span>
+                    <LinearProgress
+                      className="progress"
+                      variant="determinate"
+                      value={sum ? (numRatings.four / sum) * 100 : 0}
+                    />
+                    <span>{numRatings.four}</span>
+                  </div>
+                  <div className="star">
+                    <span>3 ★</span>
+                    <LinearProgress
+                      className="progress"
+                      variant="determinate"
+                      value={sum ? (numRatings.three / sum) * 100 : 0}
+                    />
+                    <span>{numRatings.three}</span>
+                  </div>
+                  <div className="star">
+                    <span>2 ★</span>
+                    <LinearProgress
+                      className="progress"
+                      variant="determinate"
+                      value={sum ? (numRatings.two / sum) * 100 : 0}
+                    />
+                    <span>{numRatings.two}</span>
+                  </div>
+                  <div className="star">
+                    <span>1 ★</span>
+                    <LinearProgress
+                      className="progress"
+                      variant="determinate"
+                      value={sum ? (numRatings.one / sum) * 100 : 0}
+                    />
+                    <span>{numRatings.one}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+        <Review />
       </div>
     );
   }
---
+++ client/Review.js
@@ -0,0 +1,19 @@
+const Review = () => {
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="semester">Semester</label>
+        <select id="semester">
+          <option />
+        </select>
+
+        <label htmlFor="subject">Subject</label>
+        <select id="subject">
+          <option />
+        </select>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+export default Review;
