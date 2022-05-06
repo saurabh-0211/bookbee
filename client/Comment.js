4469775b7f34330291b5722b9b4139da24551a1c
vivekvag <vivekvag281@gmail.com>
2022-05-06 16:23:33 +0530

    Changelog 3.7.17 User can now review on the book they liked.

---
+++ client/Comment.js
@@ -0,0 +1,41 @@
+import Rating from '@mui/material/Rating';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { Avatar } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  comment: {
+    width: '100%'
+  }
+}));
+
+const Comment = ({ reviews, userReview }) => {
+  console.log(reviews);
+  const classes = useStyles();
+  return (
+    <Card sx={{ width: '50%' }}>
+      {reviews.map((review) => (
+        <CardContent key={review.user}>
+          <div className={classes.avatar}>
+            <Avatar alt={review.name} src="/static/images/avatar/2.jpg" />
+            <Typography sx={{ margin: '5px ' }} variant="h5" component="span">
+              {review.name}
+            </Typography>
+          </div>
+          <Rating name="simple-controlled" size="small" value={review.rating} readOnly />
+          <Typography variant="body2" color="text.secondary">
+            {review.comment}
+          </Typography>
+        </CardContent>
+      ))}
+    </Card>
+  );
+};
+
+export default Comment;
--- client/Details.js
+++ client/Details.js
@@ -2,7 +2,8 @@ import { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import Rating from '@mui/material/Rating';
 import LinearProgress from '@mui/material/LinearProgress';
-import UserReview from './components/UserReview';
+import UserReview from './UserReview';
+import Comment from './Comment';
 class Details extends Component {
   state = { loading: true, value: 0 };
 
@@ -41,7 +42,6 @@ class Details extends Component {
       sum += numRatings[key];
     }
     let userReview = reviews.find((r) => r.user.toString() === this.props.user._id.toString());
-
     return (
       <div className="main">
         <div className="details">
@@ -56,87 +56,90 @@ class Details extends Component {
               </span>
             </div>
             <h1>{bookName}</h1>
-            <div className="list">
-              <span>Details</span>
-              <div className="book-info">
-                <div className="item">
-                  <span>Stream</span>
-                  <h3 style={{ textTransform: 'capitalize' }}>{stream}</h3>
+            <div className="list-group">
+              <div className="list">
+                <span>Details</span>
+                <div className="book-info">
+                  <div className="item">
+                    <span>Stream</span>
+                    <h3 style={{ textTransform: 'capitalize' }}>{stream}</h3>
+                  </div>
+                  <div className="item">
+                    <span>Subject</span>
+                    <h3>{subject}</h3>
+                  </div>
+                  <div className="item">
+                    <span>Author</span>
+                    <h3>{authors}</h3>
+                  </div>
+                  <div className="item">
+                    <span>Publisher</span>
+                    <h3>{publisher}</h3>
+                  </div>
                 </div>
-                <div className="item">
-                  <span>Subject</span>
-                  <h3>{subject}</h3>
-                </div>
-                <div className="item">
-                  <span>Author</span>
-                  <h3>{authors}</h3>
-                </div>
-                <div className="item">
-                  <span>Publisher</span>
-                  <h3>{publisher}</h3>
+              </div>
+              <div className="list">
+                <span>Ratings</span>
+                <div className="book-ratings">
+                  <div className="total-ratings">
+                    <h1>{rating.toFixed(1)} ★</h1>
+                    <h3>{sum} Ratings</h3>
+                  </div>
+                  <div className="star-ratings">
+                    <div className="star">
+                      <span>5 ★</span>
+                      <LinearProgress
+                        className="progress"
+                        variant="determinate"
+                        value={sum ? (numRatings.five / sum) * 100 : 0}
+                      />
+                      <span>{numRatings.five}</span>
+                    </div>
+                    <div className="star">
+                      <span>4 ★</span>
+                      <LinearProgress
+                        className="progress"
+                        variant="determinate"
+                        value={sum ? (numRatings.four / sum) * 100 : 0}
+                      />
+                      <span>{numRatings.four}</span>
+                    </div>
+                    <div className="star">
+                      <span>3 ★</span>
+                      <LinearProgress
+                        className="progress"
+                        variant="determinate"
+                        value={sum ? (numRatings.three / sum) * 100 : 0}
+                      />
+                      <span>{numRatings.three}</span>
+                    </div>
+                    <div className="star">
+                      <span>2 ★</span>
+                      <LinearProgress
+                        className="progress"
+                        variant="determinate"
+                        value={sum ? (numRatings.two / sum) * 100 : 0}
+                      />
+                      <span>{numRatings.two}</span>
+                    </div>
+                    <div className="star">
+                      <span>1 ★</span>
+                      <LinearProgress
+                        className="progress"
+                        variant="determinate"
+                        value={sum ? (numRatings.one / sum) * 100 : 0}
+                      />
+                      <span>{numRatings.one}</span>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div className="review-ratings">
-          <div className="list">
-            <span>Ratings</span>
-            <div className="book-ratings">
-              <div className="total-ratings">
-                <h1>{rating.toFixed(1)} ★</h1>
-                <h3>{sum} Ratings</h3>
-              </div>
-              <div className="star-ratings">
-                <div className="star">
-                  <span>5 ★</span>
-                  <LinearProgress
-                    className="progress"
-                    variant="determinate"
-                    value={sum ? (numRatings.five / sum) * 100 : 0}
-                  />
-                  <span>{numRatings.five}</span>
-                </div>
-                <div className="star">
-                  <span>4 ★</span>
-                  <LinearProgress
-                    className="progress"
-                    variant="determinate"
-                    value={sum ? (numRatings.four / sum) * 100 : 0}
-                  />
-                  <span>{numRatings.four}</span>
-                </div>
-                <div className="star">
-                  <span>3 ★</span>
-                  <LinearProgress
-                    className="progress"
-                    variant="determinate"
-                    value={sum ? (numRatings.three / sum) * 100 : 0}
-                  />
-                  <span>{numRatings.three}</span>
-                </div>
-                <div className="star">
-                  <span>2 ★</span>
-                  <LinearProgress
-                    className="progress"
-                    variant="determinate"
-                    value={sum ? (numRatings.two / sum) * 100 : 0}
-                  />
-                  <span>{numRatings.two}</span>
-                </div>
-                <div className="star">
-                  <span>1 ★</span>
-                  <LinearProgress
-                    className="progress"
-                    variant="determinate"
-                    value={sum ? (numRatings.one / sum) * 100 : 0}
-                  />
-                  <span>{numRatings.one}</span>
-                </div>
-              </div>
-            </div>
-          </div>
           <UserReview user={this.props.user} id={this.state._id} userReview={userReview} />
+          <Comment reviews={reviews} userReview={userReview} />
         </div>
       </div>
     );
